9aa87f75700bae9da41eea9fb2751ca7fa978ba2
gabriellacstavarengo <gabriellacstavarengo@gmail.com>
2026-08-11 13:42:28 -0300

    fix: correct import casing for components in App.jsx

--- reactjs-tailwind-icons/src/App.jsx
+++ reactjs-tailwind-icons/src/App.jsx
@@ -1,9 +1,9 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Benefits from './components/Benefits'
-import Products from './components/Products'
-import Testimonials from './components/Testimonials'
-import Footer from './components/Footer'
+import Header from './components/header'
+import Hero from './components/hero'
+import Benefits from './components/benefits'
+import Products from './components/products'
+import Testimonials from './components/testimonials'
+import Footer from './components/footer'
 
 function App() {
   return (
